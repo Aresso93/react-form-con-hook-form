57f723b071671beb9e907b066e7045490cbc47cc
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-11-14 14:27:18 +0100

    /

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { LocalizationProvider } from '@mui/x-date-pickers'
 import './App.css'
 import TravellerStepper from './components/stepper'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import TravellerForm from './components/traveller-form'
 
 function App() {
@@ -7,7 +9,9 @@ function App() {
 
   return (
     <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <TravellerStepper/>
+    </LocalizationProvider>
     
     </>
   )
--- src/components/location-form.tsx
+++ src/components/location-form.tsx
@@ -1,12 +1,42 @@
 import { useState } from "react";
-import { DatePicker } from '@mui/x-date-pickers';
+import { DatePicker } from "@mui/x-date-pickers";
 import dayjs, { Dayjs } from "dayjs";
+import { FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
 
-export function LocationForm(){
-    const [value, setValue] = useState<Dayjs | null>(dayjs('2022-04-17'));
-    <DatePicker
-          label="Controlled picker"
-          value={value}
-          onChange={(newValue) => setValue(newValue)}
-        />
+export function LocationForm() {
+  const [value, setValue] = useState<Dayjs | null>(dayjs("2022-04-17"));
+  const [age, setAge] = useState('');
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <>
+    <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Destination</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Destination"
+          onChange={handleChange}
+        >
+          <MenuItem>La Spezia</MenuItem>
+          <MenuItem>Cagliari</MenuItem>
+          <MenuItem>Mordor</MenuItem>
+        </Select>
+      </FormControl>
+      <DatePicker
+        label="Controlled picker"
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      />
+
+      <DatePicker
+        label="Controlled picker"
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      />
+    </>
+  );
 }
