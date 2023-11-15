5db16b3d96b80c68cee8ea47eb6214e2800370fa
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-11-15 16:13:19 +0100

    progressi

--- src/components/recap.tsx
+++ src/components/recap.tsx
@@ -7,8 +7,9 @@ import TravellerForm from "./traveller-form";
 
 export default function Recap(){
    const formContext = useFormContext()
-   console.log('CCCCCCCCCCC', formContext.getValues("date of birth".$L))
-
+   const stringBirthDate = formContext.getValues("date of birth").toString();
+   const stringDepartureDate = formContext.getValues("date of departure").toString();
+   const stringReturnDate = formContext.getValues("date of return").toString();
     return (
     <>
     <div className="recap-modal">
@@ -16,20 +17,28 @@ export default function Recap(){
       <span>Full name: {formContext.getValues("fullName")}</span>
       <span>Email address: {formContext.getValues("email")}</span>
       <span>Gender: {formContext.getValues("gender")}</span>
-      {/* <span>Date of birth: {formContext.getValues("date of birth".$d)}</span> */}
+      <span>Date of birth: {stringBirthDate}</span>
      <EditModal>
         <TravellerForm/>
      </EditModal>
     </div>
-
+    <div className="recap-modal">
+      Information about your travel: <br></br>
+      <span>Destination: {formContext.getValues("destination")}</span>
+      <span>Date of departure: {stringDepartureDate}</span>
+      <span>Date of return: {stringReturnDate}</span>
      <EditModal>
         <LocationForm/>
      </EditModal>
-
+     </div>
+     <div className="recap-modal">
+      Information about your preferences: <br></br>
+      <span>Method of accommodation: {formContext.getValues("accommodation")}</span>
+      <span>Selected meals: </span>
      <EditModal>
         <PreferencesForm/>
      </EditModal>
-
+      </div>
 
     </>
     )
