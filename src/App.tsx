c971e074e5822ca40e6717dbcc843f03e754889b
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-11-17 17:36:30 +0100

    /

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import "./App.css";
 import TravellerStepper from "./components/stepper";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { TextField } from "@mui/material";
 
 export interface IFormInput {
   dateOfBirth: string;
@@ -32,27 +30,15 @@ const schema = yup.object().shape({
 })
 
 function App() {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<IFormInput>({ resolver: yupResolver(schema) });
-  const onSubmit = (data: IFormInput) => console.log("Submitted!", data);
-  console.log('APP', errors)
+  const methods = useForm();
+  const onSubmit = (data) => console.log("Submitted!", data);
+
   return (
     <>
-    <TextField
-          id="test"
-          label="AAA"
-          variant="outlined"
-          {...register("test")}
-        />
-        {errors.test && <p>{errors.test.message}</p>}
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <FormProvider {...useForm()}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <TravellerStepper/>
-          <input type="submit"/>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <TravellerStepper />
           </form>
         </FormProvider>
       </LocalizationProvider>
